454d4dae92e14060c36fcce13a5ef5c2c48206fb
SED4906 <sed4906birdie@gmail.com>
2023-08-13 15:16:27 -0700

    move spawn here

--- src/exec.rs
+++ src/exec.rs
@@ -3,3 +3,12 @@ use syscalls::{syscall, Sysno};
 pub fn execute(file: &[u8], args: &[*const u8]) -> Result<usize, syscalls::Errno>{
     unsafe{syscall!(Sysno::execve,file.as_ptr(),args.as_ptr(),0)}
 }
+
+pub fn spawn(buf: &[u8], args: &[*const u8]) -> Result<usize, syscalls::Errno> {
+    let pid = unsafe{syscall!(Sysno::fork)}?;
+    if pid == 0 {
+        execute(&buf, &args)
+    } else {
+        Ok(pid)
+    }
+}
